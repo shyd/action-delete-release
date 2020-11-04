2694bf61e11b6d557542c1ae05ee8dfcd1f31e54
Dennis Schütt <dennis@shyd.de>
2020-11-04 19:36:55 +0100

    first commit with action

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Octokit } = require("@octokit/rest");
+const { env } = require ("process");
+const token = env.INPUT_GITHUB_TOKEN
+const repository = env.INPUT_REPOSITORY || env.GITHUB_REPOSITORY
+const tag = env.INPUT_GITHUB_RELEASE_TAG;
+const [owner, repo] = repository.split("/")
+
+const octokit = new Octokit({
+    auth: token
+});
+
+octokit.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag
+}).then(res => {
+    if(!res.data){
+        console.error("💡 No release found, skip delete.");
+        return
+    }
+    const release_id = res.data.id
+    octokit.repos.deleteRelease({
+        owner,
+        repo,
+        release_id
+    })
+}).catch(
+    err =>{
+        if(err.status === 404){
+            console.error("💡 No release found, skip delete.");
+            return
+        }
+        console.error("❌ Can't get latest Release");
+        console.error(err);
+    }
+)
